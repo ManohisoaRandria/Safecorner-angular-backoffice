13b10d9115b0e3eb963d1c40ff75f6837f828471
Manohisoa Randriambahiny <randriambahinymanohy@gmail.com>
2020-07-21 17:29:12 +0300

    modif protocole mbola tsy vita

--- src/app/pages/societe/protocole/modify-protocole-societe/modify-protocole-societe.component.ts
+++ src/app/pages/societe/protocole/modify-protocole-societe/modify-protocole-societe.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Societe } from '../../../../modele/societe';
+import { ApiService } from 'src/app/services/api.service';
+import { InsertService } from 'src/app/services/insert.service';
 
 @Component({
   selector: 'app-modify-protocole-societe',
@@ -8,14 +11,26 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ModifyProtocoleSocieteComponent implements OnInit {
 
-  id:string="";
-  constructor(private router:Router,private route:ActivatedRoute) { }
+  id: string = "";
+  societe: Societe;
+  constructor(private router: Router,
+    private api: ApiService,
+    private insertService: InsertService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.id=this.route.snapshot.params['id'];
+    this.id = this.route.snapshot.params['id'];
+    this.api.societeSubject.subscribe((societe: Societe[]) => {
+      this.societe = societe.find(element => element.id == this.id);
+      if (this.societe == undefined) {
+        this.router.navigate(['societe']);
+      }
+    });
+    //maka anle protocole rehetra amnio societe io
+
   }
-  goToInsert(){
-    this.router.navigate(['/add-protocole-societe',this.id]);
+  goToInsert() {
+    this.router.navigate(['/add-protocole-societe', this.id]);
   }
 
 }
