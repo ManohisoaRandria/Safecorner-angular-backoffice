92700eb555fab9cf684ee15e983db805d68ff73e
RAMANANTSEHENO Tahiana <ramanantsehenotahiana@gmail.com>
2020-07-22 00:50:53 +0300

    descente

--- src/app/pages/societe/descente/descente.component.ts
+++ src/app/pages/societe/descente/descente.component.ts
@@ -1,19 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router'; 
 
+import { ApiService } from '../../../services/api.service';
+import { GetService } from '../../../services/get.service';
+import { InsertService } from '../../../services/insert.service';
+import { Societe } from '../../../modele/societe';
+import { Protocole } from '../../../modele/protocole';
+import { NgForm, Validators } from '@angular/forms';
+import { from } from 'rxjs';
+
 @Component({
   selector: 'app-descente',
   templateUrl: './descente.component.html',
   styleUrls: ['./descente.component.css']
 })
 export class DescenteComponent implements OnInit {
-  private idSociete:string = "";
+  private id:string = "";
+  societe:Societe;
   nombreApplique:number = 0;
+  protocoleClient:Protocole[];
+  protocolePerso:Protocole[];
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute,
+              private router:Router,
+              private api:ApiService,
+              private getService:GetService,
+              private insertService:InsertService) { }
 
   ngOnInit(): void {
-    this.idSociete = this.route.snapshot.params['id'];
+    this.id = this.route.snapshot.params['id'];
+    this.api.societeSubject.subscribe((societe: Societe[]) => {
+      this.societe = societe.find(element => element.id == this.id);
+      if (this.societe == undefined) {
+        this.router.navigate(['societe']);
+      }
+    });
+    //maka anle protocole rehetra amnio societe io
+    this.getService.getProtocolesBySociete(this.id).then((res:any)=>{
+      this.protocolePerso = res.perso;
+      this.protocoleClient = res.client;
+    }).catch(err=>{
+      console.log(err);
+    })
   }
 
   onApplique($event){
@@ -25,5 +53,11 @@ export class DescenteComponent implements OnInit {
         this.nombreApplique--;
       }
     }
+  }
+
+  //insertion de l'historique de la descente
+  onInsertHistoDescente(form:NgForm){
+    this.nombreApplique = 0;
+    form.reset();
   }
 }
